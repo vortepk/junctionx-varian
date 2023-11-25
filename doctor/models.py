77510a28620a7792694b644c80ba789abd3d2a91
Roman <krp536@gmail.com>
2023-11-25 10:57:32 +0100

    Update models.py

--- doctor/models.py
+++ doctor/models.py
@@ -8,16 +8,34 @@ class Calendar(models.Model):
 
 class Patient(models.Model):
     name = models.CharField(max_length=1024)
-    family = models.CharField(max_length=1024)
-    email = models.CharField(max_length=1024)
+    last_name = models.CharField(max_length=1024)
+    sex = models.CharField(max_length=1024)
     weight = models.IntegerField()
-    fractions_number = models.IntegerField()
-    gender = models.CharField(max_length=1024)
     condition = models.CharField(max_length=1024)
     email = models.CharField(max_length=1024)
-    treat_date = models.ManyToManyField(Calendar)
+    fractions_number = models.IntegerField()
+    treatment_date = models.ManyToManyField(Calendar)
 
 
 class Machine(models.Model):
     name = models.CharField(max_length=1024)
     family = models.CharField(max_length=1024)
+
+
+class Floor(models.Model):
+    name = models.CharField(max_length=1024)
+    wheelchair_accessible = models.BooleanField()
+    has_elevator = models.BooleanField(default=False)
+
+
+class Room(models.Model):
+    number = models.CharField(max_length=1024)
+    sex = models.CharField(max_length=1024)
+    wheelchair_accessible = models.BooleanField()
+    has_bath = models.BooleanField(default=True)
+    area = models.IntegerField()  # Remoteness from treatment room zone
+
+
+class Bed(models.Model):
+    # Are there any specific beds?
+    pass
